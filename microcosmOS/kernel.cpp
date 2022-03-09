9bd6d20334c6419b60292add884a0a25bd0f4b0c
donalshortt <donalshortt@donals.eu>
2022-03-09 21:22:15 +0100

    ported to cpp

--- microcosmOS/kernel.cpp
+++ microcosmOS/kernel.cpp
@@ -1,19 +1,20 @@
 #define VIDEO_START 0xb8000
 #define VGA_LIGHT_GRAY 7
+#define VGA_RED 4
 
-static void PrintString(char *str)
+static void PrintString(const char *str)
 {
   unsigned char *video = ((unsigned char *)VIDEO_START);
   while(*str != '\0') {
     *(video++) = *str++;
-    *(video++) = VGA_LIGHT_GRAY;
+    *(video++) = VGA_RED;
   }
 }
 
-void
-Kernel_Main(void)
+extern "C" void
+kernel_main(void)
 {
-  PrintString("Hello World!");
+  PrintString("Oh hello there");
 
   while(1);
 }
